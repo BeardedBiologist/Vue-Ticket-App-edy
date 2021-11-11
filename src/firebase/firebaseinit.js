624008d3fc7b80ee04f517affae332aab4503c2d
Joshua James <joshuajames.o@me.com>
2021-11-11 11:16:38 +0100

    FIRST COMMIT - starter code from tutorial

---
+++ src/firebase/firebaseinit.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzf4Dg3SeCnTAolcEOFfmV8CS5YIO-Nws",
+  authDomain: "ticket-app-5e812.firebaseapp.com",
+  projectId: "ticket-app-5e812",
+  storageBucket: "ticket-app-5e812.appspot.com",
+  messagingSenderId: "973630178960",
+  appId: "1:973630178960:web:21d49d5ebe44b00e747a15"
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp.firestore();
